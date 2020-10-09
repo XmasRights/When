c18f23ba4324599f04dc804c6dc38bc3c6c8cde9
Christopher Fonseka <chris.fonseka@googlemail.com>
2020-10-09 18:00:17 +0100

    Xcode Project Setup

---
+++ When/WhenApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhenApp.swift
+//  When
+//
+//  Created by Christopher Fonseka on 09/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct WhenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
