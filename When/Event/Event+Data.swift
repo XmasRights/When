6600cbc4bd8f0600fd1bd86cf390b0e2042f5c10
Christopher Fonseka <chris.fonseka@googlemail.com>
2020-10-10 19:13:52 +0100

    Fixed issues with SwiftUI not updating when needed

--- When/Event/Event+Data.swift
+++ When/Event/Event+Data.swift
@@ -8,18 +8,11 @@
 import SwiftUI
 
 extension Event {
-    class Data: Identifiable {
-        let id: UUID
+    struct Data: Identifiable {
+        let id = UUID()
         var title: String
         var date: Date
         var color: Color
-
-        init(title: String, date: Date, color: Color) {
-            self.id = .init()
-            self.title = title
-            self.date = date
-            self.color = color
-        }
     }
 }
 
